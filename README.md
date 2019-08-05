2c828ba2b1f438380b0e49b5445802dc7abb2b66
Dave Kanter <moatgoat@hotmail.com>
2019-08-05 11:18:08 -0700

    Through the end of the 'A Simple Mutation' section

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # graphql-hackernews-node
 Following Tutorial on how to build a Hacker News clone in GraphQL: https://www.howtographql.com/graphql-js/0-introduction/
+
+## Also includes
+<ul>
+    <li>`graphql-yoga`, a fully-featured GQL server based on Express</li>
+
+</ul>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,66 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 // 1  Define the GraphQL schema
-const typeDefs = 
-type Query {
-    info: String!
-}
+// MOVED TO EXTERNAL FILE schema.graphql
+// const typeDefs = `
+// type Query {
+//     info: String!
+//     feed: [Link!]!
+// }
 
-// 2  Resolvers object: actual implementation of the GraphQL schema.  Notice it's similarity to // 1
+// type Mutation {
+//     post(url: String!, description: String!): Link!
+// }
+
+// type Link {
+//     id: ID!
+//     description: String!
+//     url: String!
+// }`
+
+// DUMMY LINKS
+let links= [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL'
+    },
+    {
+        id: 'link-1',
+        url: 'www.wired.com',
+        description: 'Wired Magazine'
+    }
+]
+
+let idCount = links.length
+
+// 2  Resolvers object: actual implementation of the GraphQL schema.  Notice its similarity to // 1
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links
+    },
+    // Link: { // We can remove this because the GraphQL server infers what they look like!
+    //     id: (parent) => parent.id,
+    //     description: (parent) => parent.description,
+    //     url: (parent) => parent.url
+    // }
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        }
     }
 }
 
 // 3  Bundle schema and resolvers and pass to GraphQLServer
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs:  './src/schema.graphql', // it's external now!
     resolvers,
 })
 
